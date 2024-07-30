58e0d560e8a1aef1d74f51b914440c7a0940ce02
Fer <ferp@protonmail.ch>
2024-07-30 22:45:49 +0200

    use area lights for environment ambient light without the background

--- src/script.js
+++ src/script.js
@@ -53,9 +53,9 @@ const scene = new THREE.Scene();
 // scene.background = environmentMap;
 
 // HDR (RGBE) environment map
-rgbeLoader.load('/environmentMaps/blender2k.hdr', (environmentMap) => {
+rgbeLoader.load('/environmentMaps/blenderLights2k.hdr', (environmentMap) => {
     environmentMap.mapping = THREE.EquirectangularReflectionMapping;
-    scene.background = environmentMap
+    // scene.background = environmentMap
     scene.environment = environmentMap
 })
 
